fcf0621c34e8584baf9c377a46b59ea754afd093
unknown <kelanrafferty@gmail.com>
2022-11-10 17:30:46 +0000

    project Initialized

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5;
+    public Rigidbody rb;
+
+    float horizontalInput;
+    public float horizontalMultiplier = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        //Fixed update runs 50 times per second, which gives the physics engine a stable based to work from rather than
+        //A frame rate dependant update method.
+
+        Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
+        Vector3 horizontalMove = transform.right * horizontalInput * speed * Time.fixedDeltaTime * horizontalMultiplier;
+        rb.MovePosition(rb.position + forwardMove + horizontalMove);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+}
